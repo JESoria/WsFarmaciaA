b6950ccd6635ff1cbc5dbac9b4f285df245d4691
XiomaraAlarcon <xio.carballo51@outlook.es>
2019-05-03 11:31:45 -0600

    alarcon

--- FarmaciaA/Controllers/DetailController.cs
+++ FarmaciaA/Controllers/DetailController.cs
@@ -20,29 +20,17 @@ namespace FarmaciaA.Controllers
             {
                 db.SUCURSAL_PRODUCTO.Where(x => x.ID_SUCURSAL_PRODUCTO == data.idSucursalProducto).ToList().ForEach(x =>
                 {
-                    db.PRODUCTO.Where(y => y.ID_PRODUCTO == x.ID_PRODUCTO).ToList().ForEach(y =>
+                    detalle = new DetailModel()
                     {
-                        db.PRESENTACION.Where(z => z.ID_PRESENTACION == y.ID_PRESENTACION).ToList().ForEach(z =>
-                        {
-                            db.CATEGORIA.Where(c => c.ID_CATEGORIA == y.ID_CATEGORIA).ToList().ForEach(c =>
-                            {
-                                db.LABORATORIO.Where(l => l.ID_LABORATORIO == y.ID_LABORATORIO).ToList().ForEach(l =>
-                                {
-                                    detalle = new DetailModel()
-                                    {
-                                        producto = y.PRODUCTO1,
-                                        presentacion = z.PRESENTACION1,
-                                        fechaVencimiento = x.FECHA_VENCIMIENTO.ToShortDateString(),
-                                        laboratorio = l.LABORATORIO1.Trim(),
-                                        principiosActivos = y.DESCRIPCION,
-                                        categoria = c.CATEGORIA1,
-                                        precio = Convert.ToDouble(x.PRECIO),
-                                        existencia = x.EXISTENCIA
-                                    };
-                                });
-                            });
-                        });
-                    });
+                        producto = x.PRODUCTO.PRODUCTO1,
+                        presentacion = x.PRODUCTO.PRESENTACION.PRESENTACION1,
+                        fechaVencimiento = x.FECHA_VENCIMIENTO.ToShortDateString(),
+                        laboratorio = x.PRODUCTO.LABORATORIO.LABORATORIO1.Trim(),
+                        principiosActivos = x.PRODUCTO.DESCRIPCION.Trim().Replace("\t", " "),
+                        categoria = x.PRODUCTO.CATEGORIA.CATEGORIA1,
+                        precio = Convert.ToDouble(x.PRECIO),
+                        existencia = x.EXISTENCIA
+                    };
                 });
 
                 return Ok(detalle);
--- FarmaciaA/Controllers/SearchByDrugstoreController.cs
+++ FarmaciaA/Controllers/SearchByDrugstoreController.cs
@@ -10,30 +10,29 @@ namespace FarmaciaA.Controllers
     public class SearchByDrugstoreController : ApiController
     {
         [HttpPost]
-        public async Task<IHttpActionResult> SearchDrugstore(SearchModel data)
+        public IHttpActionResult SearchDrugstore(SearchModel data)
         {
             List<ProductSearchModel> lista = new List<ProductSearchModel>();
-            
+
 
             using (FarmaciaAEntities db = new FarmaciaAEntities())
             {
                 var Farmacia = db.FARMACIA.FirstOrDefault();
-                db.SUCURSAL_PRODUCTO.OrderBy(x => x.ID_SUCURSAL_PRODUCTO).ToList().ForEach(x =>
+
+                db.SUCURSAL_PRODUCTO.Where(x => x.ID_SUCURSAL == data.idSucursal && x.PRODUCTO.PRODUCTO1.ToLower().Contains(data.producto.ToLower())).ToList().ForEach(x =>
                 {
-                    db.PRODUCTO.Where(y => y.ID_PRODUCTO == x.ID_PRODUCTO && y.PRODUCTO1.Contains(data.producto) && x.ID_SUCURSAL == data.idSucursal).ToList().ForEach(z =>
-                    {
-                        lista.Add(new ProductSearchModel
+                    lista.Add(new ProductSearchModel
                         {
                             idSucursalProducto = x.ID_SUCURSAL_PRODUCTO,
                             idSucursal = x.ID_SUCURSAL,
-                            producto = z.PRODUCTO1,
+                            producto = x.PRODUCTO.PRODUCTO1,
                             precio = Convert.ToDecimal(x.PRECIO),
                             sucursal = x.SUCURSAL.SUCURSAL1,
                             latitud = x.SUCURSAL.LATITUD,
                             longitud = x.SUCURSAL.LONGITUD,
                             direccion = x.SUCURSAL.DIRECCION,
                             idFarmacia = Convert.ToInt32(Farmacia.ID_FARMACIA)
-                        });
+                        
                     });
                 });
                 return Ok(lista.OrderBy(x => x.precio));
